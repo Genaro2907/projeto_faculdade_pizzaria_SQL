2c29498cf41d75095bdaa732ef6ced8b7f5af58e
Gabriel Genaro <g.genaro.contato@gmail.com>
2025-09-22 15:42:13 -0300

    New Menu

--- Pizzaria/main.c
+++ Pizzaria/main.c
@@ -6,6 +6,69 @@
 #include "bebidas.h"
 #include "pedidos.h"
 
+void menuPizzas(sqlite3 *db) {
+    int opcao;
+    do {
+        printf("\n=== MENU PIZZAS ===\n");
+        printf("1. Cadastrar Pizza\n");
+        printf("2. Listar Pizzas\n");
+        printf("3. Excluir Pizza\n");
+        printf("0. Voltar\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1: cadastrarPizza(db); break;
+            case 2: listarPizzas(db); break;
+            case 3: excluirPizza(db); break;
+            case 0: break;
+            default: printf("Opcao invalida!\n");
+        }
+    } while (opcao != 0);
+}
+
+void menuBebidas(sqlite3 *db) {
+    int opcao;
+    do {
+        printf("\n=== MENU BEBIDAS ===\n");
+        printf("1. Cadastrar Bebida\n");
+        printf("2. Listar Bebidas\n");
+        printf("3. Excluir Bebida\n");
+        printf("0. Voltar\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1: cadastrarBebida(db); break;
+            case 2: listarBebidas(db); break;
+            case 3: excluirBebida(db); break;
+            case 0: break;
+            default: printf("Opcao invalida!\n");
+        }
+    } while (opcao != 0);
+}
+
+void menuPedidos(sqlite3 *db) {
+    int opcao;
+    do {
+        printf("\n=== MENU PEDIDOS ===\n");
+        printf("1. Realizar Pedido\n");
+        printf("2. Listar Pedidos\n");
+        printf("3. Excluir Pedido\n");
+        printf("0. Voltar\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1: realizarPedido(db); break;
+            case 2: listarPedidos(db); break;
+            case 3: excluirPedido(db); break;
+            case 0: break;
+            default: printf("Opcao invalida!\n");
+        }
+    } while (opcao != 0);
+}
+
 int main() {
     sqlite3 *db;
     int rc = sqlite3_open("pizzaria.db", &db);
@@ -16,59 +79,24 @@ int main() {
     }
     
     printf("Conectado ao banco de dados SQLite com sucesso!\n");
-    
     criarTabelas(db);
     
     int opcao;
-    
     do {
         printf("\n=== SISTEMA PIZZARIA ===\n");
-        printf("1. Cadastrar Pizza\n");
-        printf("2. Cadastrar Bebida\n");
-        printf("3. Listar Pizzas\n");
-        printf("4. Listar Bebidas\n");
-        printf("5. Realizar Pedido\n");
-        printf("6. Listar Pedidos\n");
-        printf("7. Excluir Pizza\n");
-        printf("8. Excluir Bebida\n");
-        printf("9. Excluir Pedido\n");
+        printf("1. Pizzas\n");
+        printf("2. Bebidas\n");
+        printf("3. Pedidos\n");
         printf("0. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
-        
+
         switch(opcao) {
-            case 1:
-                cadastrarPizza(db);
-                break;
-            case 2:
-                cadastrarBebida(db);
-                break;
-            case 3:
-                listarPizzas(db);
-                break;
-            case 4:
-                listarBebidas(db);
-                break;
-            case 5:
-                realizarPedido(db);
-                break;
-            case 6:
-                listarPedidos(db);
-                break;
-            case 7:
-                excluirPizza(db);
-                break;
-            case 8:
-                excluirBebida(db);
-                break;
-            case 9:
-                excluirPedido(db);
-                break;
-            case 0:
-                printf("Saindo...\n");
-                break;
-            default:
-                printf("Opcao invalida!\n");
+            case 1: menuPizzas(db); break;
+            case 2: menuBebidas(db); break;
+            case 3: menuPedidos(db); break;
+            case 0: printf("Saindo...\n"); break;
+            default: printf("Opcao invalida!\n");
         }
     } while (opcao != 0);
     
